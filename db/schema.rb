a0da65fdf5d556258675d6fad31cf16c5224898b
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-10 02:33:19 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_051749) do
+ActiveRecord::Schema.define(version: 2019_11_09_173008) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "body"
   end
 
 end
