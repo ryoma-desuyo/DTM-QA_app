95179815b1101410664104d50ae4c30786ed46aa
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-16 22:17:26 +0900

    Misspelling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destory]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.order(created_at: :desc)
