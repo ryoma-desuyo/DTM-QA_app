fd5c344ba922afcd3046e9e2e7b781582c7e4605
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-21 22:01:51 +0900

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 1000 }
 
 end
