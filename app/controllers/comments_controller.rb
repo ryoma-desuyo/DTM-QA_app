d73acb9e29d55cfb11fc68e73688331b0f31349d
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-21 15:19:32 +0900

    Create comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_to root_path, alert: 'コメントできませんでした'
+    end
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
