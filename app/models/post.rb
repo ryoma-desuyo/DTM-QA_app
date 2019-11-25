82c2876ecdcd062cd9b5a052b77aa64c90af0586
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-25 18:04:28 +0900

    relation post_tag, tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :delete_all
+  has_many :post_tag_relations, dependent: :delete_all
+  has_many :tags, through: :post_tag_relations 
 
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true, length: { maximum: 1000 }
