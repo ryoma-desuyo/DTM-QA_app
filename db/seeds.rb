548715a1647f22f980652b1f72c972623da88c92
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-25 18:05:06 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env == 'development'
+  (1..100).each do |i|
+      Post.create(title: "タイトル#{i}", body: "本文#{i}")
+  end
+
+  Tag.create([
+      { name: 'Garage Band' },
+      { name: 'Studio One' },
+      { name: 'Logic Pro' },
+      { name: 'Ableton Live' },
+      { name: 'Cubaase' },
+      { name: 'FL Studio' },
+      { name: 'Pro tools' },
+      { name: 'Sonar' }
+  ])
+end
