1d62694baded3c0d7b5bdd390f82d203e98f83e0
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-28 05:59:16 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       get 'index_list'
     end
     resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show]
   resources :mypages, only: [:index, :edit, :update]
