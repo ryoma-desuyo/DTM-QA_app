4bf7aea47a6d20b160adefa81115ede2c2ce99f5
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-27 11:32:28 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,12 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true, length: { maximum: 1000 }
 
+  mount_uploader :image, ImagesUploader
+  mount_uploader :video, VideosUploader
+
+  def self.search(search)
+    return Post.all unless search
+    Post.where(['title LIKE  ?', "%#{search}%"])
+  end
+
 end
